6a3d02f81a2459c147ee79d63b26149f71deca27
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-30 13:25:40 +0200

    feat(DetailsPage): added details page of blog (no img)

--- frontend/src/pages/DetailsPage.jsx
+++ frontend/src/pages/DetailsPage.jsx
@@ -1,6 +1,20 @@
+import { useParams } from "react-router-dom";
+import { useContext } from "react"
+import { postData } from '../App'
+
 const DetailsPage = () => {
+
+    const posts = useContext(postData)
+    
+    console.log(posts && posts[0])
+    const postIndex = parseInt(useParams().id)
+    
     return ( 
-        <h1>DetailsPage</h1>
+        <div>
+            {/* <img style={{width: "100px"}} src={''}></img> */}
+            <h1>{posts && posts[postIndex].title}</h1>
+            <p>{posts && posts[postIndex].message}</p>
+        </div>
      );
 }
  
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,17 +8,19 @@ import { v4 as uuidv4 } from 'uuid'
 const Home = () => {
 
     const posts = useContext(postData)
-    
-   
+    console.log(posts)
 
-    /* if (!posts) return */
+
+    if (!posts) return
 
     return (
         <div >
             <h1>HOMEEEEEEEEEE</h1>
         <Link to={'/admin'}>link to admin page</Link>
         {
+        
         posts?.map((post,index) => {
+            
             return (
             <SinglePost 
             key={uuidv4()}
