e61399cfd53a5c365d9a612d4833efa948f63adf
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-30 11:09:00 +0200

    added button to home on admin page

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,6 +1,7 @@
 import { useState} from 'react'
 import InputForm from '../components/InputForm'
-
+import { Link } from 'react-router-dom'
+import Home from './Home'
 
 const AdminPage = () => {
     
@@ -10,8 +11,8 @@ const AdminPage = () => {
     
     return ( 
         <div>
-
             <InputForm setPosts={setPosts} />
+            <Link to={'/'}>Go to Blog</Link>
         </div>
      );
 }
